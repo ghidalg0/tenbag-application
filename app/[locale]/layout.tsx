99f2431c5e322167c20fbfe5721b5ec04b5aec38
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2024-09-15 08:56:38 +0200

    added intl

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import ".././globals.css";
+import { Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/toaster";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+
+export const metadata: Metadata = {
+  title: "Tenbag App",
+  description: "Tenbag App",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default async function RootLayout({
+  children,
+  params: { locale },
+}: Readonly<{
+  children: React.ReactNode;
+  params: { locale: string };
+}>) {
+  const messages = await getMessages();
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+          <Toaster />
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
--- components/custom/Navbar.tsx
+++ components/custom/Navbar.tsx
@@ -3,7 +3,6 @@
 import { deleteAuthCookie } from "@/actions/cookies";
 import { Button } from "../ui/button";
 import { usePathname, useRouter } from "next/navigation";
-import Link from "next/link";
 import {
   ChartCandlestick,
   EyeIcon,
@@ -11,31 +10,11 @@ import {
   LogOut,
   MessageCircle,
 } from "lucide-react";
-
-const tabs = [
-  {
-    href: "/dashboard",
-    label: "Dashboard",
-    icon: <LayoutDashboardIcon size={20} />,
-  },
-  {
-    href: "/dashboard/investments",
-    label: "Investments",
-    icon: <ChartCandlestick size={20} />,
-  },
-  {
-    href: "/dashboard/screeners",
-    label: "Screeners",
-    icon: <EyeIcon size={20} />,
-  },
-  {
-    href: "/dashboard/forum",
-    label: "Forum",
-    icon: <MessageCircle size={20} />,
-  },
-];
+import { useTranslations } from "next-intl";
+import { Link } from "@/i18n/routing";
 
 export const Navbar = () => {
+  const t = useTranslations("Navbar");
   const router = useRouter();
   const pathname = usePathname();
 
@@ -44,12 +23,37 @@ export const Navbar = () => {
     router.push("/login");
   };
 
+  const tabs = [
+    {
+      href: "/dashboard",
+      label: t("dashboard"),
+      icon: <LayoutDashboardIcon size={20} />,
+    },
+    {
+      href: "/dashboard/investments",
+      label: t("investments"),
+      icon: <ChartCandlestick size={20} />,
+    },
+    {
+      href: "/dashboard/screeners",
+      label: t("screeners"),
+      icon: <EyeIcon size={20} />,
+    },
+    {
+      href: "/dashboard/forum",
+      label: t("forum"),
+      icon: <MessageCircle size={20} />,
+    },
+  ];
+
   return (
     <nav className="pt-8 h-screen border-e-2 w-[240px] fixed top-0 px-4 pb-4 flex flex-col justify-between">
       <h1 className="text-center font-black text-4xl">10Bag</h1>
       <div className="grow space-y-2 flex flex-col pt-16">
         {tabs.map((tab) => {
-          const isActive = tab.href === pathname;
+          const isActive = tab.href === pathname.replace(/^\/[a-z]{2}/, "");
+
+          console.log(pathname.split("/"));
 
           if (isActive) {
             return (
