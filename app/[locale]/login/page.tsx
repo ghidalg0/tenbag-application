41ef5e901b7554139c0ccb376c15d6c64e0d85c9
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2024-09-16 08:07:13 +0200

    fixed : types on login page

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -1,6 +1,10 @@
 import { LoginForm } from "@/modules/auth/LoginForm";
 
-export default function Home({ params: { locale } }) {
+export default function Home({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
   const loginUrl = `${process.env.BACK_URL}/auth/login`;
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-4 w-full">
